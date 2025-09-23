0c753eddbee7a7ef324d554e7da1284850a682b5
m-golang <mirjalol.golang@gmail.com>
2025-09-23 12:23:39 +0500

    feat (config): integrate static config service into startasena

--- cmd/startasena.go
+++ cmd/startasena.go
@@ -1,13 +1,15 @@
 package cmd
 
 import (
+	"github.com/asenalabs/asena/internal/config"
 	"github.com/asenalabs/asena/pkg/logger"
 	"go.uber.org/zap"
 )
 
 var (
-	version = "0.0.1"
-	env     = "development" //	development | production
+	version             = "0.0.2"
+	env                 = "development" //	development | production
+	asenaConfigFilePath = "asena.yaml"
 )
 
 func StartAsena() {
@@ -15,5 +17,13 @@ func StartAsena() {
 	logger.IntiFallbackZapLogger()
 	logg := logger.Get()
 
+	//	Load Asena Configurations
+	asenaConfigService, err := config.NewAsenaConfigService(asenaConfigFilePath, logg)
+	if err != nil {
+		logg.Fatal("Failed to initialize Asena configurations", zap.Error(err))
+	}
+	asenaCfg := asenaConfigService.Get()
+
 	logg.Info("Starting asena", zap.String("version", version), zap.String("env", env))
+	logg.Info("Asena configuration", zap.Bool("enable https", *asenaCfg.Asena.EnableHTTPS))
 }
