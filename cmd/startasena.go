d538a687404ccb9a996b1a3e6d653017118814b6
m-golang <mirjalol.golang@gmail.com>
2025-10-03 15:15:45 +0500

    feat (cmd): integrate dynamic config loader with hot-reload and bump version to 0.0.7 (#18)

--- cmd/startasena.go
+++ cmd/startasena.go
@@ -1,18 +1,27 @@
 package cmd
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/asenalabs/asena/internal/config"
 	"github.com/asenalabs/asena/pkg/logger"
 	"go.uber.org/zap"
 )
 
 var (
-	version             = "0.0.6"
-	env                 = "development" //	development | production
-	asenaConfigFilePath = "asena.yaml"
+	version               = "0.0.7"
+	env                   = "development" //	development | production
+	asenaConfigFilePath   = "asena.yaml"
+	dynamicConfigFilePath = "dynamic.yaml"
 )
 
 func StartAsena() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	//	Initialize basic logger
 	logger.IntiFallbackZapLogger()
 	logg := logger.Get()
@@ -28,6 +37,18 @@ func StartAsena() {
 	logger.InitProductionZapLogger(env, asenaCfg.Log)
 	logg = logger.Get()
 	defer logger.Sync()
+
+	//	Load dynamic configurations
+	dynamicConfigService, err := config.NewDynamicConfigService(ctx, dynamicConfigFilePath, logg)
+	if err != nil {
+		logg.Fatal("Failed to initialize dynamic configurations", zap.Error(err))
+	}
+
+	go func() {
+		for _ = range dynamicConfigService.Updates() {
+
+		}
+	}()
 
 	logg.Info("Starting asena", zap.String("version", version), zap.String("env", env))
 	logg.Info("Asena configuration", zap.Bool("enable https", *asenaCfg.Asena.EnableHTTPS))
