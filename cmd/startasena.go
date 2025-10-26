1958150dfa3665b646d58bd34e45362b2e770964
m-golang <mirjalol.golang@gmail.com>
2025-10-26 16:32:20 +0500

    chore (pre-release): bump version to v0.1.6 #36

--- cmd/startasena.go
+++ cmd/startasena.go
@@ -17,7 +17,7 @@ import (
 )
 
 var (
-	version               = "0.1.5"
+	version               = "0.1.6"
 	env                   = "development" //	development | production
 	asenaConfigFilePath   = "asena.yaml"
 	dynamicConfigFilePath = "dynamic.yaml"
