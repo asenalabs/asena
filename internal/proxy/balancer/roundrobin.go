eec248eb3b29daeae9d35bfa8938bac10904b77a
m-golang <mirjalol.golang@gmail.com>
2026-08-14 22:14:54 +0500

    balancer: make Next request-aware, add Done completion hook (#64)

--- internal/proxy/balancer/roundrobin.go
+++ internal/proxy/balancer/roundrobin.go
@@ -1,8 +1,10 @@
 package balancer
 
 import (
+	"net/http"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"github.com/asenalabs/asena/internal/config"
 )
@@ -19,7 +21,9 @@ func NewRoundRobin(servers []*config.ServerCfg) *RoundRobin {
 	}
 }
 
-func (rr *RoundRobin) Next() *config.ServerCfg {
+// Next ignores r: Round Robin doesn't care which client is asking,
+// it just rotates throught the server list
+func (rr *RoundRobin) Next(r *http.Request) *config.ServerCfg {
 	rr.mu.RLock()
 	defer rr.mu.RUnlock()
 
@@ -31,3 +35,7 @@ func (rr *RoundRobin) Next() *config.ServerCfg {
 	pos := atomic.AddUint64(&rr.counter, 1)
 	return rr.servers[pos%uint64(l)]
 }
+
+// Done is a no-op: Round Robin doesn't track active connections or response
+// time, so there's nothing to update when a request finishes.
+func (rr *RoundRobin) Done(server *config.ServerCfg, drtn time.Duration, err error) {}
